8ec69e1a08d2aba39db9fa098986cd5a0adb1c79
Panos Mavrogiorgos <pmav99@gmail.com>
2014-04-04 22:31:07 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@
 prezto_dir=~/.prezto
 dot_dir=~/dotfiles
 old_dir=~/dotfiles_old
-
-files="bashrc vimrc zshrc zpreztorc"
 ##########
 
 print "1.  Creating $old_dir for backup of any existing dotfiles in ~ ..."
@@ -24,14 +22,16 @@ print "4.  Backup the existing prezto dotfiles in homedir to $old_dir directory"
 print "    and then create symlinks of the default prezto files from the"
 print "    prezto installation folder to ~."
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    print "      Processing:   $rcfile"
-    mv ~/.${rcfile:t} ~/dotfiles_old/
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+for filename in ~/.prezto/runcoms/^README.md(.N); do
+    print "      Processing:   $filename"
+    if ls ~/.${filename:t} &> /dev/null; then
+        mv ~/.${filename:t} ~/dotfiles_old/
+    fi
+    ln -s "$filename" "${ZDOTDIR:-$HOME}/.${filename:t}"
 done
 print "    Done"
 
-print "5.  Replace prezto dotfiles with custom version from $dir directory."
+print "5.  Replace prezto dotfiles with custom version from $dot_dir directory."
 print "    This operation will remove the default prezto files."
 for filename in $dot_dir/z*; do
     print "      Processing:   $filename"
@@ -50,11 +50,8 @@ fi
 ln -s $dot_dir/vim/vimrc-linux.vim ~/.vimrc
 print "6.b Handling Vundle"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-#vim +PluginInstall +qall
-
-# you complete me
-#print "6.c Handling Vundle"
-#cd ~/.vim/bundle/YouCompleteMe
-##./install.sh --clang-completer
-#cd ~
 print "    Done"
+
+print ""
+print "Finished"
+print "Log out and log in again and you shall be ready to go!"
