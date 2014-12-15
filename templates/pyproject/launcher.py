7ce7f442c2c32dfd00cba16ab199899fe2f4fb92
Panos Mavrogiorgos <pmav99@gmail.com>
2014-12-15 21:44:24 +0200

    Add a python project template

---
+++ templates/pyproject/launcher.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#author: Panagiotis Mavrogiorgos
+#email: gmail, pmav99
+
+"""
+script description
+
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import logging
+import logging.config
+import argparse
+
+
+from project_name import Config, OptionsBase
+
+
+class Options(OptionsBase):
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(
+            description=__doc__,
+            prog=__file__,
+            epilog="")
+
+        parser.add_argument(
+            '--version',
+            action='version',
+            version='%(prog)s',
+        )
+
+        parser.add_argument(
+            '-c', '--config-file',
+            dest="config_file",
+            action='store',
+            default="config.json",
+            help='The location of the configuration file.',
+        )
+
+        options = parser.parse_args()
+        return options
+
+
+def main():
+    logger = logging.getLogger()
+    logger.info("Logging works OK")
+
+
+if __name__ == "__main__":
+    options = Options()
+    config = Config()
+    # parse CLI options and the configuration file
+    options.initialize()
+    config.initialize(options.config_file)
+    # initialize logging
+    logging.config.dictConfig(config["logging"])
+
+    main()
---
+++ templates/pyproject/project_name/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#author: Panagiotis Mavrogiorgos
+#email: gmail, pmav99
+# project_name/__init__.py
+
+"""
+project_name
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+
+from .config import Config, OptionsBase
